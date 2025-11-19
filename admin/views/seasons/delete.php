b4f996b0002c7639f33d6cd3f70349afa890697e
Grandin Harrison <grandin.harrison@gmail.com>
2025-11-18 17:19:15 -0800

    Fix syntax error in seasons delete form

--- admin/views/seasons/delete.php
+++ admin/views/seasons/delete.php
@@ -3,7 +3,8 @@
     <h1><?php _e('Delete Season', 'the-rink-society'); ?></h1>
     <div class="notice notice-warning"><p><strong><?php _e('Warning:', 'the-rink-society'); ?></strong> <?php _e('Delete this season?', 'the-rink-society'); ?></p></div>
     <p><strong><?php echo esc_html($season->name); ?></strong></p>
-    <form method="post"><<?php wp_nonce_field('trs_season_action'); ?>
+    <form method="post">
+        <?php wp_nonce_field('trs_season_action'); ?>
         <input type="submit" name="trs_delete_season" class="button button-primary" value="<?php _e('Yes, Delete', 'the-rink-society'); ?>">
         <a href="<?php echo admin_url('admin.php?page=trs-seasons'); ?>" class="button"><?php _e('Cancel', 'the-rink-society'); ?></a>
     </form>
